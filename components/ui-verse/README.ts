d0e375c2c42d27848e204a0103cf1fcb28dfde3a
Henrik Teschke <henrikteschke02@gmail.com>
2026-07-15 20:24:54 +0200

    feat: add animejs, sonner toast and ui-verse directory

---
+++ components/ui-verse/README.ts
@@ -0,0 +1,8 @@
+// Lege hier manuell kopierte JSX/Tailwind-Komponenten von uiverse.io ab.
+// Beispiel-Import in einer Page:
+//   import { MyCoolButton } from "@/components/ui-verse/MyCoolButton"
+//
+// Alle Komponenten hier sollten "use client" nutzen, da sie oft interaktive
+// CSS-Animationen oder Event-Handler verwenden.
+
+export {}
